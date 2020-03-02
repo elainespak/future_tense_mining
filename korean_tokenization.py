54dac0757ff147d637a3387889e26e3422e44cd1
Elaine Pak <elainespak@gmail.com>
2020-03-02 22:14:04 +0900

    Update korean_tokenization.py

--- korean_tokenization.py
+++ korean_tokenization.py
@@ -14,10 +14,10 @@ import re
 from nltk.tokenize import sent_tokenize
 from konlpy.tag import Okt
 
-local_path = 'C:/Users/elain/Desktop/Future Tense Mining/'
+local_path = 'C:/Users/elain/Desktop/논문-Future Tense Mining/'
 data_folder = 'data/'
 output_folder = 'output_data/'
-file_name = 'kospi200_2015_2019'
+file_name = 'kospi200_2011_2015'
 dat = pd.read_excel(local_path + data_folder + file_name + '.xlsx')
 raw_text = dat['Text']
 
@@ -26,28 +26,55 @@ raw_text = dat['Text']
 ### --- *** --- Text Pre-processing --- *** --- ###
 ############## ---------------------- ###############
 
-# Things to remove from the text
-messy_items = ['\n', ',', '(', ')', '%', '~']
-
-def clean_text(text):
-    for i in messy_items:
-        text = text.replace(i, '')    
-    text = re.sub('\d+', '', text)
-    return text
-
 # Use nltk to split sentences (works great on Korean as well)
 token_text = raw_text.apply(lambda x: sent_tokenize(x))
 
-# Select relevant, proper sentences and pre-process
-sent_text = []
+# Use extra hand-made parser
+def additional_sent_tokenize(text):
+    '''
+    Some bullet points, which should be taken as separate sentences,
+    are not accounted for by the nltk sentence tokenizer.
+    This additional tokenizer will take care of such bullet points.
+    '''
+    if '※' in text:
+        text_list = text.split('※')
+    elif '* ' in text:
+        text_list = text.split('* ')
+    else:
+        text_list = [text]
+    return text_list
+
+additional_token_text =[]
 
 for t in token_text:
+    temp_list = []
+    for sent in t:
+        temp_list.append(additional_sent_tokenize(sent))
+    additional_token_text.append([item for sublist in temp_list for item in sublist])
+
+# Select relevant, proper sentences and pre-process
+def clean_text(text):
+    '''
+    Clean the messy raw text
+    1) Remove \n, 전자공시시스템 dart.fss.or.kr, Page #
+    2) Remove white space before and after the input text
+    '''
+    text = re.sub("\\n|(전자공시시스템 dart\.fss\.or\.kr)?(\n)?(Page \d{1,2})?(?=\s)",
+                  " ",
+                  text)
+    text = text.strip()
+    return text
+
+sent_text = []
+
+for t in additional_token_text:
     relevant_sentences = []
     
-    for sent in t:
-        if sent.endswith('니다.'):
-            sent = clean_text(sent)
-            relevant_sentences.append(sent)
+    for s in t:
+        if s.endswith('니다.'):
+            s = clean_text(s)
+            s = s.strip()
+            relevant_sentences.append(s)
             
     sent_text.append(relevant_sentences)
 
@@ -63,10 +90,13 @@ dat.to_csv(local_path + output_folder + file_name + '_parsed.csv', index=False)
 # Create object instance
 okt = Okt()
 print(sent_text[0][0])
-print(okt.nouns(sent_text[0][0])) # works!
+print(okt.nouns(sent_text[0][0])) # Not bad
 
 # Select relevant sentences and pre-process
 def konlpy_noun(sentences):
+    '''
+    Return a list of nouns from a list of sentences
+    '''
     return [okt.nouns(sent) for sent in sentences]
 
 sent_text = pd.Series(sent_text)
@@ -76,52 +106,3 @@ noun_text = sent_text.apply(lambda x: konlpy_noun(x))
 dat['Nouns'] = noun_text
 dat.to_csv(local_path + output_folder + file_name + '_nouns.csv', index=False)
 
-
-
-
-
-############## ---------------------- ###############
-### --- *** --- Trash codes --- *** --- ###
-############## ---------------------- ###############
-
-text = raw_text.apply(lambda x: clean_text(x))
-
-
-text = raw_text.apply(lambda x: x.replace('\n', ''))
-text = text.apply(lambda x: x[10:]) # remove 'II. 사업의 내용 '
-text = text.apply(lambda x: re.sub('[\d]{+}', '', x))
-
-# Test nltk on Korean to split sentences
-sent_tokenize(text[0]) # pretty good!
-
-
-i = 0
-for t in list(text):
-    if 'II. 사업의 내용 ' in t:
-        i+=1
-print(i)
-
-
-from soynlp.noun import LRNounExtractor
-noun_extractor = LRNounExtractor()
-nouns = noun_extractor.train_extract(sent_text[0][0])
-
-
-from soynlp.utils import DoublespaceLineCorpus
-from soynlp.noun import LRNounExtractor_v2
-
-noun_extractor = LRNounExtractor_v2(verbose=True)
-noun_extractor.train_extract(sent_text[0][:5])
-
-
-
-from soynlp.word import WordExtractor
-
-word_extractor = WordExtractor(min_frequency=7,
-    min_cohesion_forward=0.05, 
-    min_right_branching_entropy=0.0
-)
-word_extractor.train(sent_text[0]) # list of str or like
-words = word_extractor.extract()
-
-print(words)
